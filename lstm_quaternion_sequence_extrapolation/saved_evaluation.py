Corrected accuracy calculation
* Added calculation of multiple accuracy levels in one take
* Upgraded evaluation flags
* Added QLSTM QAL model
* Updated environment.yml

lstm_quaternion_sequence_extrapolation/saved_evaluation.py
@@ -16,10 +16,15 @@ input_size = 4          # Quaternion
 sequence_length = 100   # Frames
 batch_size = 10
 
-show_evaluation = True
+is_qal_loss = False
+show_evaluation = False
+calculate_accuracy = True
+max_acc_round_point = 7
+
 # model_path = rf"./models/lstm_mse_batch10_epochs3.pth"
-# model_path = rf"./models/lstm_qal_batch10_epochs3.pth"
-model_path = rf"./models/qlstm_mse_batch10_epochs3.pth"
+model_path = rf"./models/lstm_qal_batch10_epochs3.pth"
+# model_path = rf"./models/qlstm_mse_batch10_epochs3.pth"
+# model_path = rf"./models/qlstm_qal_batch10_epochs3.pth"
 # model_path = rf"./models/VectorizedQLSTM_mse_batch10_epochs3.pth"
 # model_path = rf"./models/VectorizedQLSTM_qal_batch10_epochs3.pth"
 
@@ -34,7 +39,7 @@ dataset = RotationDataset(training_path, labels_path, input_size, sequence_lengt
 
 
 # 2. Splitting dataset
-print("2. Splitting dataset")
+print("\n2. Splitting dataset")
 training_size = int(0.8 * len(dataset))
 test_size = len(dataset) - training_size
 training_dataset, test_dataset = random_split(dataset, [training_size, test_size])
@@ -57,20 +62,26 @@ print(f"Model type: {type(model)}")
 
 
 # 5. Criterion
-print("5. Creating criterion")
-print("Evaluation criterion: QALLoss function")
-criterion_eval = nn.MSELoss()
-# criterion_eval = rm.QALLoss()
+print("\n5. Creating criterion")
+if is_qal_loss:
+    print("Evaluation criterion: QALLoss function")
+    criterion_eval = rm.QALLoss()
+else:
+    print("Evaluation criterion: MSELoss function")
+    criterion_eval = nn.MSELoss()
+
+
+def evaluation_print(message: str):
+    if show_evaluation:
+        print(message)
 
 
 # 6. Test and evaluation
-print("6. Starting evaluation")
+print("\n6. Starting evaluation")
 with torch.no_grad():
     test_loss = []
+    correct_predictions = [0 for _ in range(max_acc_round_point + 1)]
     n_samples = 0
-
-    round_point = 339
-    correct_predictions = 0
 
     for (rotations, labels) in test_loader:
         rotations = rotations.to(device)
@@ -80,26 +91,33 @@ with torch.no_grad():
 
         test_loss.append(criterion_eval(output, labels).item())
 
-        # Show evaluation results
-        if show_evaluation:
-            output = output.tolist()
-            labels = labels.tolist()
-            n_samples += 1
+        # Calculating accuracy
+        output = output.tolist()
+        labels = labels.tolist()
+        n_samples += 1
 
-            for i in range(len(labels)):
-                #print(f"output: {output[i]}, expected: {labels[i]}")
-                w_acc = int(round(output[i][0], round_point) == round(labels[i][0], round_point))
-                i_acc = int(round(output[i][1], round_point) == round(labels[i][1], round_point))
-                j_acc = int(round(output[i][2], round_point) == round(labels[i][2], round_point))
-                k_acc = int(round(output[i][3], round_point) == round(labels[i][3], round_point))
+        for i in range(len(labels)):
+            evaluation_print(f"\noutput: {output[i]}, expected: {labels[i]}")
 
-                current_predictions = (w_acc + i_acc + j_acc + k_acc)
-                correct_predictions += current_predictions
-                #print(f"acc: {((100 * current_predictions) / (input_size)):.2f}%")
-            #print(f"loss: {test_loss[n_samples - 1]}")
+            if calculate_accuracy:
+                for acc_i in range(max_acc_round_point + 1):
+                    w_acc = int(round(output[i][0], acc_i) == round(labels[i][0], acc_i))
+                    i_acc = int(round(output[i][1], acc_i) == round(labels[i][1], acc_i))
+                    j_acc = int(round(output[i][2], acc_i) == round(labels[i][2], acc_i))
+                    k_acc = int(round(output[i][3], acc_i) == round(labels[i][3], acc_i))
+
+                    current_predictions = (w_acc + i_acc + j_acc + k_acc)
+                    correct_predictions[acc_i] += current_predictions
+                    evaluation_print(f"accuracy [{acc_i}]: {((100 * correct_predictions[acc_i]) / (input_size)):.2f}%")
+
+        evaluation_print(f"loss: {test_loss[n_samples - 1]}")
 
     test_loss = np.array(test_loss)
     loss_mean = np.mean(test_loss)
     loss_std = np.std(test_loss)
-    accuracy = (100 * correct_predictions) / (input_size * batch_size * n_samples)
-    print(f'Loss mean: {loss_mean:.7f}, loss std: {loss_std:.7f}, acc: {accuracy:.2f}%')
+    print("\nSUMMARY")
+    print(f'Loss mean: {loss_mean:.7f}, loss std: {loss_std:.7f}')
+    
+    if calculate_accuracy:
+        for i in range(len(correct_predictions)):
+            print(f'accuracy [{i}]: {((100 * correct_predictions[i]) / (input_size * batch_size * n_samples)):.2f}')